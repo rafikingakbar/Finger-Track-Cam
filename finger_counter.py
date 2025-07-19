6e6c9b6880a3bd39a3b760c5eb08bfbe9efc3343
Rafi King Akbar <rafikingakbar@gmail.com>
2025-07-19 15:16:19 +0700

    Nambah tombol mirror dan finger count sementara

--- finger_counter.py
+++ finger_counter.py
@@ -9,25 +9,57 @@ hands = mp_hands.Hands(static_image_mode=False,
                        min_tracking_confidence=0.7)
 mp_draw = mp.solutions.drawing_utils
 
-# Ambil video dari kamera
 cap = cv2.VideoCapture(0)
 
-# ID ujung jari
 finger_tips = [4, 8, 12, 16, 20]
 
+# Status toggle
+mirror_mode = False
+counting_enabled = True
+
+# Tombol (x1, y1, x2, y2)
+# Tombol (x1, y1, x2, y2) - Pojok kanan atas
+btn_mirror = (460, 20, 610, 60)
+btn_toggle = (460, 70, 610, 110)
+
+def draw_buttons(img, mirror_mode, counting_enabled):
+    # Mirror Button
+    mirror_color = (0, 255, 0) if mirror_mode else (0, 0, 255)
+    cv2.rectangle(img, btn_mirror[:2], btn_mirror[2:], mirror_color, -1)
+    cv2.putText(img, 'Mirror', (btn_mirror[0]+10, btn_mirror[1]+25),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255), 2)
+
+    # Count Toggle Button
+    toggle_color = (0, 255, 0) if counting_enabled else (0, 0, 255)
+    cv2.rectangle(img, btn_toggle[:2], btn_toggle[2:], toggle_color, -1)
+    cv2.putText(img, 'Count', (btn_toggle[0]+10, btn_toggle[1]+25),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255), 2)
+
+
+def check_touch(pos, box):
+    x, y = pos
+    return box[0] < x < box[2] and box[1] < y < box[3]
+
+last_touch_time = 0
+
+import time
 while True:
     success, img = cap.read()
     if not success:
         break
 
+    if mirror_mode:
+        img = cv2.flip(img, 1)
+
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(img_rgb)
-
     total_fingers = 0
+
+    draw_buttons(img, mirror_mode, counting_enabled)
 
     if results.multi_hand_landmarks:
         for hand_landmarks, hand_label in zip(results.multi_hand_landmarks, results.multi_handedness):
-            label = hand_label.classification[0].label  # 'Left' atau 'Right'
+            label = hand_label.classification[0].label
             lm_list = []
             h, w, _ = img.shape
 
@@ -35,39 +67,41 @@ while True:
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 lm_list.append((cx, cy))
 
-            fingers = []
+            # Deteksi tekan tombol (pakai ujung jari telunjuk)
+            tip_index = lm_list[8]
 
-            # Thumb (ibu jari): deteksi horizontal berdasarkan tangan kiri/kanan
-            if label == 'Right':
-                if lm_list[4][0] < lm_list[3][0]:
-                    fingers.append(1)
+            current_time = time.time()
+            if current_time - last_touch_time > 1:  # debouncing 1 detik
+                if check_touch(tip_index, btn_mirror):
+                    mirror_mode = not mirror_mode
+                    last_touch_time = current_time
+                elif check_touch(tip_index, btn_toggle):
+                    counting_enabled = not counting_enabled
+                    last_touch_time = current_time
+
+            if counting_enabled:
+                fingers = []
+
+                # Thumb
+                if label == 'Right':
+                    fingers.append(1 if lm_list[4][0] < lm_list[3][0] else 0)
                 else:
-                    fingers.append(0)
-            else:
-                if lm_list[4][0] > lm_list[3][0]:
-                    fingers.append(1)
-                else:
-                    fingers.append(0)
+                    fingers.append(1 if lm_list[4][0] > lm_list[3][0] else 0)
 
-            # 4 jari lainnya: deteksi vertikal
-            for tip_id in finger_tips[1:]:
-                if lm_list[tip_id][1] < lm_list[tip_id - 2][1]:
-                    fingers.append(1)
-                else:
-                    fingers.append(0)
+                # 4 jari lainnya
+                for tip_id in finger_tips[1:]:
+                    fingers.append(1 if lm_list[tip_id][1] < lm_list[tip_id - 2][1] else 0)
 
-            total_fingers += fingers.count(1)
+                total_fingers += fingers.count(1)
 
-            # Gambar tangan
             mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-    # Tampilkan jumlah jari di layar
-    cv2.putText(img, f'Jari total: {total_fingers}', (10, 70),
-                cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 255), 3)
+    # Tampilkan jumlah jari jika diaktifkan
+    if counting_enabled:
+        cv2.putText(img, f'Jari total: {total_fingers}', (10, 70),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 255), 3)
 
     cv2.imshow("Finger Counter (2 Hands)", img)
-
-    # Tekan 'q' untuk keluar
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
